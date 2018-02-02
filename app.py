300a5b540fdf406a6bdcbdc80ae51ea4b509d40f
tung <tung110197@gmail.com>
2018-02-02 22:15:12 +0700

    handle 404 error, return a json form

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from flask import Flask, jsonify
 from flask import abort
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -29,5 +30,10 @@ def get_task(task_id):
         if len(task) == 0:
             abort(404)
         return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
     app.run(debug=True)
