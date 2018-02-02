8d34c15bbd307bcdd9e2bef15ab50deecf6b40b9
tung <tung110197@gmail.com>
2018-02-02 22:22:56 +0700

    create POST method

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify
 from flask import abort
 from flask import make_response
+from flask import request
 
 app = Flask(__name__)
 
@@ -26,14 +27,27 @@ def get_tasks():
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-        task = [task for task in tasks if task['id'] == task_id]
-        if len(task) == 0:
-            abort(404)
-        return jsonify({'task': task[0]})
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 @app.errorhandler(404)
 def not_found(error):
-        return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201   
 
 if __name__ == '__main__':
     app.run(debug=True)
